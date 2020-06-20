895150cc45bbef127713b66c804230919045f7b9
mourya.polaka@gmail.com <mourya.polaka@gmail.com>
2020-06-20 15:21:00 +1000

    update preprocess.py, feature_encode and clean_data function

--- app.py
+++ app.py
@@ -6,20 +6,30 @@ import data_exploration as DE
 
 def main():
     st.title("DSAI Modelling Platform")
-    st.sidebar.image('res/images/dsai_v2.png',width=270)
-    st.sidebar.title("Build your Model")
     st.markdown("Build a Classification or Regression Model")
+    st.sidebar.image('res/images/dsai_v2.png',width=300)
+    st.sidebar.markdown("Build a classification or regression model using DSAI platform.")
 
+    #Import dataset
     st.header("1. Import Dataset")
     data_frame = load_data()
+    temp_df = data_frame.copy()
 
+    #Exploratory analysis section
     st.header("2. Exploratory Analysis")
     df_stats = DE.Exploration.show_basic_eda(data_frame)
 
+    #Data cleaning section
     st.header("3. Pre Process")
-    replace_missing_values = PP.MissingValues.replace_missing_values(data_frame)
-    feature_encoding = PP.LabelEncode.feature_encode(data_frame)
+    clean_missing_values = PP.PreProcess.replace_missing_values(data_frame)
+    feature_encoding = PP.PreProcess.feature_encode(data_frame)
 
+    if st.button("Clean Data", key = 'clean_data'):
+        clean_btn = PP.PreProcess.clean_data(temp_df,clean_missing_values,feature_encoding)
+        st.subheader("Pre Processed Data Frame")
+        st.dataframe(clean_btn)
+    
+    #Feature selection section
     st.header("4. Feature Selection")
     feature_selection_plots = DE.PlotCharts.feature_plots(data_frame)
 
--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,45 @@
 import streamlit as st
 import pandas as pd
+from sklearn import preprocessing
 
-class MissingValues:
+class PreProcess:
 
     def replace_missing_values(data):
+
+        replace_options = ["-","Mean","Median","Mode"]
         st.subheader("Replace Missing Values")
         columns = st.multiselect("Select columns", (data.columns))
-        options = st.selectbox("Replace with:", ("Choose an option","Mean","Median","Mode"))
+        option = st.selectbox("Replace with:", (replace_options))
 
-        if options == 'Mean':
+        return option, columns
+
+    def feature_encode(data):
+
+        st.subheader("Label Encoder")
+        column_selection = st.multiselect("Select columns to encode:", (data.columns))
+
+        return column_selection
+
+    def clean_data(df, missing_value_options, encode_columns):
+
+        data_frame = df
+        #Replacing missing values
+        option = missing_value_options[0]
+        columns = missing_value_options[1]
+
+        if option == 'Mean':
             for cols in columns:
-                data.fillna(data[cols].mean())
-        elif options == 'Median':
+                data_frame[cols].fillna(data_frame[cols].mean(), inplace=True)
+        elif option == 'Median':
             pass
-        elif options == 'Mode':
+        elif option == 'Mode':
             pass
-        
-        return data
 
-class LabelEncode:
+        #Label encoding
+        encode_col_selection = encode_columns
+        le = preprocessing.LabelEncoder()
 
-    def feature_encode(dataset):
-        st.subheader("Label Encode")
-        encode_columns = st.multiselect("Select columns to encode:", (dataset.columns))
+        for cols in encode_col_selection:
+            data_frame['{}_encoded'.format(cols)] = le.fit_transform(data_frame[cols])
+
+        return data_frame
